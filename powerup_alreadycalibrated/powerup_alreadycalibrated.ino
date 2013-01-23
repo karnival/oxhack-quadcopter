055fbb53f0d929517cc6de622f689961fc35c2bd
karnival <karnival@github>
2013-01-23 21:00:10 +0000

    Rewrote control code for rotors that are already calibrated. Is now vaguely sensible in its approach.

--- powerup_alreadycalibrated/powerup_alreadycalibrated.ino
+++ powerup_alreadycalibrated/powerup_alreadycalibrated.ino
@@ -1,13 +1,12 @@
 // RC motor controller
 
-int signalPinLB = 3; // Set PWM outputs.
-int signalPinRB = 9;
-int signalPinLF = 10;
-int signalPinRF = 11;
+int rotorPin1 = 3; // Set PWM outputs.
+int rotorPin2 = 9;
+int rotorPin3 = 10;
+int rotorPin4 = 11;
 
 int lowDuty  = 32; // Lowest duty cycle is 12.5%, scaled to 0-255 for analogWrite().
 int highDuty = 64; // Highest duty cycle is 25%, scaled to 0-255 for analogWrite().
-int medDuty = 40;
 
 boolean rotorsPowered = false; // Check rotors' state.
 
@@ -18,10 +17,10 @@ void setup(){
   Serial.begin(9600);
   Serial.println("Serial begun");
   
-  pinMode(signalPinLB, OUTPUT);
-  pinMode(signalPinRB, OUTPUT);
-  pinMode(signalPinLF, OUTPUT);
-  pinMode(signalPinRF, OUTPUT);
+  pinMode(rotorPin1, OUTPUT);
+  pinMode(rotorPin2, OUTPUT);
+  pinMode(rotorPin3, OUTPUT);
+  pinMode(rotorPin4, OUTPUT);
   
   pinMode(13, OUTPUT);
   
@@ -38,21 +37,12 @@ void loop(){
     else{
       Serial.println("Rotors lost power?");
     }
-    
-    /*analogWrite(signalPinLB, highDuty);
-    delay(5000);
-    analogWrite(signalPinLB, lowDuty);
-    delay(5000);*/
-    
 }
 
-void powerUpRotor() { // Power up the rotors, then idle them at low duty cycle. Our ESCs, we believe, go between duty cycle 5% and 10%. Frequency is believed to be 500Hz, which is analogWrite() default.
+void powerUpRotor() { // Power up the rotors, then idle them at 0.5 effort.
   long startTime = millis(); // Initialise time at which rotors have started to undergo powerup sequence. Might need to change this to look at start time for individual rotors.
   
-  analogWrite(signalPinLB, lowDuty); // Set 12.5% duty cycle PWM wave for rotors.
-  analogWrite(signalPinRB, lowDuty);
-  analogWrite(signalPinLF, lowDuty);
-  analogWrite(signalPinRF, lowDuty);
+  setRotor(0, 0.0); // Set all rotors to minimal effort
   
   digitalWrite(13, LOW);
   
@@ -62,11 +52,8 @@ void powerUpRotor() { // Power up the rotors, then idle them at low duty cycle. 
   }
 
   digitalWrite(13, LOW);
-
-  analogWrite(signalPinLB, medDuty); // Set 25% duty cycle PWM wave for rotors.
-  analogWrite(signalPinRB, medDuty);
-  analogWrite(signalPinLF, medDuty);
-  analogWrite(signalPinRF, medDuty);
+  
+  setRotor(0, 0.5); // Set all rotors to half effort
 
   
   curTime = millis();
@@ -74,12 +61,44 @@ void powerUpRotor() { // Power up the rotors, then idle them at low duty cycle. 
     curTime = millis();
   }
 
-
-  /*analogWrite(signalPinLB, lowDuty); // Set 12.5% duty cycle PWM wave for rotors. They will idle at this until we enter the main loop.
-  analogWrite(signalPinRB, lowDuty);
-  analogWrite(signalPinLF, lowDuty);
-  analogWrite(signalPinRF, lowDuty);
-  */
   digitalWrite(13, HIGH);
 
+}
+
+void setRotor(int rotor, float effort) {
+  if (effort < 0.0) {
+    effort = 0.0;
+  }
+  if (effort > 1.0) {
+    effort = 1.0;
+  }
+  
+  if (0 <= effort <= 1) {
+    int duty = lowDuty + (highDuty-lowDuty)*effort; // Duty cycle, scaled to 0-255 for analogWrite()
+    
+    Serial.print("effort is");
+    Serial.println(effort);
+    
+    Serial.print("duty is");
+    Serial.println(duty);
+    
+    if      (rotor == 1) {
+      analogWrite(rotorPin1, duty);
+    }
+    else if (rotor == 2) {
+      analogWrite(rotorPin2, duty);
+    }
+    else if (rotor == 3) {
+      analogWrite(rotorPin3, duty);
+    }
+    else if (rotor == 4) {
+      analogWrite(rotorPin4, duty);
+    }
+    else if (rotor == 0) {
+      analogWrite(rotorPin1, duty);
+      analogWrite(rotorPin2, duty);
+      analogWrite(rotorPin3, duty);
+      analogWrite(rotorPin4, duty);
+    }
+  }
 }
